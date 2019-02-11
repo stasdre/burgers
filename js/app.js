25f77ec12e79bed137cba9e8d2e35c37c4716719
Stan <stasdre@gmail.com>
2019-02-11 14:44:34 +0200

    team accordion JS

--- js/app.js
+++ js/app.js
@@ -11,3 +11,31 @@ closeHamburger.addEventListener('click', function(e){
   e.preventDefault();
   header.classList.remove('header_active');
 });
+
+
+const teamItems = document.querySelectorAll('.team__item');
+
+for(const item of teamItems){
+  item.addEventListener('click', e => {
+    e.preventDefault();
+    const curItem = e.currentTarget;
+    const wrap = curItem.querySelector('.team__wrap');
+    const textBlock = wrap.firstElementChild;
+    const height = textBlock.getBoundingClientRect().height;
+
+    if(curItem.classList.contains('team__item_active')){
+      wrap.style.height = 0;
+      curItem.classList.remove('team__item_active');  
+    }else{
+
+      teamItems.forEach(element => {
+        element.classList.remove('team__item_active');
+        element.querySelector('.team__wrap').style.height = 0;
+      });
+
+      wrap.style.height = `${height}px`;
+      curItem.classList.add('team__item_active');  
+    }
+    //console.log(height);
+  });
+}
